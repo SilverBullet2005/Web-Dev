5352c2724b850b68977c559272067d2f9a3b4d89
Kushagra Goel <KushagraGoel2005@gmail.com>
2024-07-24 18:58:59 +0530

    Dynamic Web Dev

--- Tailwind/tailwind.config.js
+++ Tailwind/tailwind.config.js
@@ -2,8 +2,27 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue': '#1fb6ff',
+        'purple': '#7e5bef',
+        'pink': '#ff49db',
+        'orange': '#ff7849',
+        'green': '#13ce66',
+        'yellow': '#ffc82c',
+        'gray-dark': '#273444',
+        'gray': '#8492a6',
+        'gray-light': '#d3dce6',
+      },
+    },
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976 px',
+      xl: '1440 px',
+    },
   },
+
   plugins: [],
 }
 
